57fd3a02b178e7bd8f9754ce49cb1dd1a92ab15e
tracer-nju <tracer@nju.edu.cn>
2022-03-04 20:13:30 +0800

    Static hostname: steve-MacBookPro          Icon name: computer-laptop            Chassis: laptop         Machine ID: 53eae4f13b8a4338bbc7c797dda60d96            Boot ID: f111879a801f4bb0aa71cf992032792c   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-30-generic       Architecture: x86-64  20:13:30 up 6 days, 20:55,  9 users,  load average: 0.31, 0.21, 0.16

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -129,16 +129,16 @@ void get_process(processes * p) {
             perror(NULL);
             exit(EXIT_FAILURE);
         }
-        printf("debug: stat = %s\n", buf);
+        // printf("debug: stat = %s\n", buf);
 
         char * delimiter = " ";
         char * token = strtok(buf, delimiter);
         for (int i = 0; i < 3; i++) {
             token = strtok(NULL, delimiter);
-            printf("debug: token = %s\n", token);
+            // printf("debug: token = %s\n", token);
             if (0==i){
                 strncpy(p->p_array[p->p_num].cmd, token, strlen(token));
-                printf("debug: cmd = %s, len = %zu, token = %s, token_len = %zu\n", p->p_array[p->p_num].cmd, strlen(p->p_array[p->p_num].cmd), token, strlen(token)  );
+                // printf("debug: cmd = %s, len = %zu, token = %s, token_len = %zu\n", p->p_array[p->p_num].cmd, strlen(p->p_array[p->p_num].cmd), token, strlen(token)  );
             }
             if (2==i) {
                 p->p_array[p->p_num].ppid = atoi(token);
