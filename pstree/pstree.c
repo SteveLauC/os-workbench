0b02ab3da54ff650d1fb26f0d4ec016a03f99eaa
tracer-nju <tracer@nju.edu.cn>
2022-03-04 15:43:24 +0800

    Static hostname: steve-MacBookPro          Icon name: computer-laptop            Chassis: laptop         Machine ID: 53eae4f13b8a4338bbc7c797dda60d96            Boot ID: f111879a801f4bb0aa71cf992032792c   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-30-generic       Architecture: x86-64  15:43:24 up 6 days, 16:25,  9 users,  load average: 0.25, 0.42, 0.50

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 #include <assert.h>
+#include <getopt.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+typedef int bool;
+typedef struct {
+    bool show_pid;
+    bool numeric_sort;
+    bool version; 
+}options;
+
+options get_options(int ac, char *av[]) {
+    int option = -1;
+
+    const char * short_option = ":pnv";
+    const struct option long_opton[4] = {
+        {"show-pids", 0, NULL, 'p'},
+        {"numeric-sort", 0, NULL, 'n'},
+        {"version", 0, NULL, 'v'},
+        {NULL, 0, NULL, 0},
+    };
+
+    while (-1 != (option = getopt_long(ac, av, short_option, &long_opton[0], NULL))) {
+        switch option{
+            case 'p':
+                break;
+            case 'p':
+                break;
+            case 'p':
+                break;
+            case 'p':
+                break;
+            case 'p':
+                break;
+        }
+    }
+}
+int main(int ac, char *av[]) {
+
 }
