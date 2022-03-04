4b8ca51c2e2ff7c35f0f61d682ac2e58ce897ca6
tracer-nju <tracer@nju.edu.cn>
2022-03-04 16:57:37 +0800

    Static hostname: steve-MacBookPro          Icon name: computer-laptop            Chassis: laptop         Machine ID: 53eae4f13b8a4338bbc7c797dda60d96            Boot ID: f111879a801f4bb0aa71cf992032792c   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-30-generic       Architecture: x86-64  16:57:37 up 6 days, 17:39,  9 users,  load average: 0.38, 0.44, 0.53

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <getopt.h>
+#include <sys/types.h>
 
+#define MAX_PROC 20000
+
+// type of cli options
 typedef int bool;
 typedef struct {
     bool show_pid;
@@ -10,16 +14,17 @@ typedef struct {
     bool version; 
 }options;
 
-void show_options(options * opt) {
-    printf("show pid: %d\nnumeric sort: %d\nversion: %d\n", opt->show_pid, opt->numeric_sort, opt->version);
-}
+// void show_options(options * opt) {
+//     printf("show pid: %d\nnumeric sort: %d\nversion: %d\n", opt->show_pid, opt->numeric_sort, opt->version);
+// }
+
+// parse the cli options
 options get_options(int ac, char *av[]) {
     options opt = {
         0,
         0,
         0,
     };
-    int option = -1;
 
     const char * short_option = ":pnv";
     const struct option long_opton[4] = {
@@ -29,6 +34,7 @@ options get_options(int ac, char *av[]) {
         {NULL, 0, NULL, 0},
     };
 
+    int option = -1;
     while (-1 != (option = getopt_long(ac, av, short_option, &long_opton[0], NULL))) {
         switch (option){
             case 'p':
@@ -52,7 +58,31 @@ options get_options(int ac, char *av[]) {
     }
     return opt;
 }
+
+// definition of the process
+typedef struct {
+    char * cmd[256];
+    pid_t pid;
+    int parent_index;
+} process;
+
+// definition of the list of processes
+typedef struct {
+    process p_array[MAX_PROC];
+    int p_num;
+} processes;
+
+/*
+  read subdirectories of `/proc` and file `/proc[pid]/stat` to get the processes info
+  format of `/proc/[pid]/stat`: pid comm state ppid ... (other irrelevant fields are omitted)
+*/
+int get_process(processes * p) {
+
+}
+
 int main(int ac, char *av[]) {
     options opt = get_options(ac, av);
     show_options(&opt);
+
+    return 0;
 }
